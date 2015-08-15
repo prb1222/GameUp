fc3e4276cb4c22f39784d162027acb335d6267ab
Peter Benavides <peter.r.benavides@gmail.com>
2015-08-15 04:39:22 -0700

    Add seed data

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development, :test do
   gem 'web-console', '~> 2.0'
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'faker'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+bowser = User.create(username: "bowser", password: "bowser")
+
+users = [];
+10.times do |i|
+  users << User.create(username: Faker::Internet.user_name,
+                       password: Faker::Internet.password(6))
+end
+
+bowser_army = bowser.groups.create(title: "BOWSER'S ARMY",
+                     description: "It's the koopa king's army!",
+                     location: "Mushroom Kingdom",
+                     member_name: "koopas",
+                     owner_id: 1)
+GroupMembership.create(user_id: 1, group_id: 1)
+
+# bowser_army.events.create(title: "Kidnap Peach",
+#                           location: "Peach's castle",
+#                           description: "Stealth raid to take Princess Peach from her castle.",
+#                           group_id: 1)
