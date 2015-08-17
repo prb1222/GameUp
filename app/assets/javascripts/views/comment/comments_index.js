0485cfed2e75e4c62857fbd34633316f274f4354
Peter Benavides <peter.r.benavides@gmail.com>
2015-08-17 03:30:28 -0700

    Working comment form

--- app/assets/javascripts/views/comment/comments_index.js
+++ app/assets/javascripts/views/comment/comments_index.js
@@ -42,10 +42,13 @@ GameUp.Views.CommentsIndex = Backbone.CompositeView.extend({
     var comment = new GameUp.Models.Comment({event_id: this.event.get('id')});
     this.formView = new GameUp.Views.CommentForm({model: comment, collection: this.collection, verb: "Create"});
     this.addSubview('div.comment-form', this.formView);
+    this.$el.find('.comment-textarea').focus();
+    this.$el.find('.new-comment').css("display","none");
   },
 
   removeCommentForm: function () {
     this.formView && this.removeSubview('div.comment-form', this.formView);
+    this.$el.find('.new-comment').css("display","block");
   },
 
   handleClick: function (event) {
