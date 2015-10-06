b2b1c92fdb3e999bb3ef53c4f1a42e69996dce3c
Peter Benavides <peter.r.benavides@gmail.com>
2015-10-05 20:03:45 -0700

    Changed geocoder server to Telize

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,3 +1,4 @@
 # Geocoder.configure(:lookup => :google_premier, :api_key => "AIzaSyC7qxk1lv03jlFc-XwItY5f5bSGmw7zHl8")
 Geocoder::Configuration.timeout = 15
+Geocoder.configure(ip_lookup: :telize)
 Geocoder.configure(:always_raise => :all)
