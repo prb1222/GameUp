4e9f270e97c63095a3cb48b32f7680daae043c40
Peter Benavides <pbenavides@catbird.com>
2018-04-02 02:52:22 -0700

    set geocoder creds

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,4 +1,4 @@
-# Geocoder.configure(:lookup => :google_premier, :api_key => "AIzaSyC7qxk1lv03jlFc-XwItY5f5bSGmw7zHl8")
+Geocoder.configure(:lookup => :google, :api_key => "AIzaSyC7qxk1lv03jlFc-XwItY5f5bSGmw7zHl8", :use_https => true)
 Geocoder::Configuration.timeout = 15
 Geocoder.configure(ip_lookup: :telize)
 Geocoder.configure(:always_raise => :all)
